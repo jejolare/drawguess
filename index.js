e08b944fa0aac1a3763412e50181186246eb9acb
andrewprog97 <andrewprog97@example.com>
2022-08-14 14:09:48 +0300

    add backend change room parameters emitter

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function makeCode(length) {
         password: "12345677",
         database: "gameinfo"
     });
+    await connection.connect();
     const roomsInfo = {};
     // {
     //     9EJoij704p7g: {
@@ -46,18 +47,40 @@ function makeCode(length) {
         //     }
         // });
         socket.on("create-room", async (data) => {
+            let gameCode;
+            let identifier;
             while (true) {
-                connection.connect();
-                let newCode = makeCode(12);
-                let [ selected ] = await connection.query("SELECT * FROM games WHERE gameCode=?", [newCode]);
+                // await connection.connect();
+                gameCode = makeCode(12);
+                let [ selected ] = await connection.query("SELECT * FROM games WHERE gameCode=?", [gameCode]);
                 if (!selected[0]) {
-                    await connection.query("INSERT INTO games(gameCode, rounds, drawingTime, status, players) VALUES (?, ?, ?, ?, ?)", [newCode, 3, 80, "inMenu", JSON.stringify([data.nickname])]);
-                    connection.end();
-                    socket.join(newCode);
+                    while (true) {
+                        identifier = makeCode(12);
+                        let [ identifiers ] = await connection.query("SELECT * FROM identifiers WHERE identifier=?", [identifier]);
+                        if (!(identifiers[0])) {
+                            await connection.query("INSERT INTO identifiers(identifier, nickname) VALUES (?, ?)", [identifier, data.nickname]);
+                            break;
+                        }
+                    }
+                    await connection.query("INSERT INTO games(gameCode, rounds, drawingTime, status, players) VALUES (?, ?, ?, ?, ?)", [gameCode, 3, 80, "inMenu", JSON.stringify([identifier])]);
+                    // await connection.end();
+                    socket.join(gameCode);
                     break;
                 }
             }
-            socket.emit("creating-menu-loaded");
+            socket.emit("creating-menu-loaded", { code: gameCode, identifier: identifier });
+        });
+        socket.on("change-room-parameters", (data) => {
+            // await connection.connect();
+            let [ players ] = await connection.query("SELECT players FROM games WHERE gameCode=?", [data.gameCode]);
+            if (!!(JSON.parse(players[0])[0]) && (JSON.parse(players[0])[0] == data.identifier)) {
+                await connection.query("UPDATE games SET rounds=?, drawingTime=? WHERE gameCode=?", [data.rounds, data.drawingTime, data.gameCode]);
+                io.to(gameCode).emit("room-parameters-changed", { rounds: data.rounds, drawingTime: data.drawingTime });
+                // await connection.end();
+            }
+        });
+        socket.on("join", (data) => {
+
         });
     });
     server.listen(3001);
